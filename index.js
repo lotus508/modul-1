9eb85c1806f9b6d95c360c21855ff43eb06809c0
Жас <zhasulan@bk.ru>
2022-03-21 13:26:32 +0600

    Добавил 2-й вид товара

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+{
+    const product = 'Картофель';
+    const amount = 10;
+    const category = 'овощи';
+    const price = 12.5;
+
+    console.log('Наименование товара: ', product);
+    console.log('Сумма товара: ', amount * price);
+}
+
+{
+    const product = 'smart проектор philips';
+    const amount = 5;
+    const category = 'видео техника';
+    const price = 1200;
+}
